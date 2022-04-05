c8b3dbef234917dec8ec77077dc41237f885ea3d
LeeSeongho <hoanart@naver.com>
2022-04-05 16:23:39 +0900

    add Annonation

--- Rope/Assets/Scripts/RopePhysics.cs
+++ Rope/Assets/Scripts/RopePhysics.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 public class RopePhysics : MonoBehaviour {
+    [Header("Anchor")]
     public Transform startTr;
+
     public Transform endTr;
-    
+
     [Header("Rope")]
     public LineRenderer lineRenderer;
+
+    //Rope Width
     public float width = 0.1f;
-    
-    public float segmentCount =15;
-    public float elasticity=15.0f;
-    //More than 1
-    public float segmentLength=10.0f;
-    
+
+    public float segmentCount = 15;
+
+    //The more increase, elasticity is low.
+    public float elasticity = 15.0f;
+
+    //More than 1.
+    public float segmentLength = 10.0f;
+
+    //A list of all segment; 
     [SerializeField]
     public List<Segment> segments = new List<Segment>();
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -34,88 +42,102 @@ public class RopePhysics : MonoBehaviour {
         {
             ApplyFixedPosition();
         }
-        
     }
 
     public void Update()
     {
         DrawRope();
+        
+        //Bottom object is positioned by segments' last position.
         endTr.position = segments[segments.Count - 1].pos;
-        endTr.LookAt(segments[segments.Count-2].pos);
+        endTr.LookAt(segments[segments.Count - 2].pos);
     }
 
-    public void SetVertex()
+    /// <summary>
+    /// Each of segments position is set.
+    /// </summary>
+    private void SetVertex()
     {
         Vector3 segmentPos = startTr.position;
-        
+
         for (int i = 0; i < segmentCount; i++)
         {
-            Segment currentSegment = new Segment(segmentPos); 
+            Segment currentSegment = new Segment(segmentPos);
             segments.Add(currentSegment);
+            /// y of segment's position goes down by segmentLength.
             segmentPos.y -= segmentLength;
         }
     }
 
-    public void SimulateRope()
+    /// <summary>
+    /// Simulate Rope using velocity-Verlet algorithm.
+    /// </summary>
+    private void SimulateRope()
     {
         //Define gravity and deltaTime.
         Vector3 gravity = new Vector3(0, -9.81f, 0);
         float deltaTime = Time.fixedDeltaTime;
-        
+
+        //Set start constraint.
         var startSegment = segments[0];
         startSegment.pos = startTr.position;
         segments[0] = startSegment;
-        
+
         for (int i = 1; i < segmentCount; i++)
         {
             var currentPos = segments[i];
             //Calculate velocity
             Vector3 velocity = segments[i].pos - segments[i].prevPos;
             currentPos.prevPos = segments[i].pos;
-            
-            Vector3 newVelocity = velocity+ 0.5f * gravity * deltaTime;
+            //use velocity-Verlet.
+            Vector3 newVelocity = velocity + 0.5f * gravity * deltaTime;
             currentPos.pos += newVelocity;
-            currentPos.velocity += gravity * deltaTime;
             segments[i] = currentPos;
-            
         }
     }
 
-    public void ApplyFixedPosition()
+    /// <summary>
+    /// Move the moved segment to correct position.
+    /// </summary>
+    private void ApplyFixedPosition()
     {
+        //First segment is fixed.
         segments[0] = new Segment(segments[0]);
-        for (int i = 0; i < segments.Count-1; i++)
+
+        for (int i = 0; i < segments.Count - 1; i++)
         {
             var top = segments[i];
             var bottom = segments[i + 1];
-            float dist = (top.pos-bottom.pos).magnitude;
-            
+            //Distance from top to bottom.
+            float dist = (top.pos - bottom.pos).magnitude;
+
             // How long stretch or compress.
             float diff = dist - segmentLength;
+            //Direction to position of top.
             Vector3 dir = (top.pos - bottom.pos).normalized;
-
-            Vector3 distortion = diff * dir;
-
-
-
-                if (i == 0)
+            //value that deviate from segment's length.
+            Vector3 offset = diff * dir;
+            
+            //In order to correct position, position of top go down and position of bottom go up.
+            if (i == 0)
             {
-                bottom.pos += distortion*0.5f;
+                bottom.pos += offset * 0.5f;
                 segments[i + 1] = bottom;
             }
             else
             {
-                top.pos -= distortion*0.5f;
+                top.pos -= offset * 0.5f;
                 segments[i] = top;
-                bottom.pos += distortion*0.5f;
+                bottom.pos += offset * 0.5f;
                 segments[i + 1] = bottom;
             }
         }
-        
     }
-    
-    //public void ApplyConstraint
-    public void DrawRope()
+
+    /// <summary>
+    /// Draw rope using LineRenderer.
+    /// </summary>
+    private void DrawRope()
     {
         Vector3[] vertices = new Vector3[segments.Count];
         for (int i = 0; i < vertices.Length; i++)
@@ -125,7 +147,7 @@ public class RopePhysics : MonoBehaviour {
 
         lineRenderer.startWidth = width;
         lineRenderer.endWidth = width;
-        
+
         lineRenderer.positionCount = vertices.Length;
         lineRenderer.SetPositions(vertices);
     }
--- Rope/Assets/Scripts/Segment.cs
+++ Rope/Assets/Scripts/Segment.cs
@@ -2,66 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [Serializable]
-public struct Segment { 
+public struct Segment {
     public Vector3 prevPos;
     public Vector3 pos;
-    public Vector3 velocity;
-  public Segment(Vector3 pos)
-  {
-    prevPos = pos;
-    this.pos = pos;
-    velocity = Vector3.zero;
-  }
 
-  public Segment(Vector3 pos, Vector3 prevPos,Vector3 velocity)
-  {
-      this.pos = pos;
-      this.prevPos = prevPos;
-      this.velocity = velocity;
-  }
+    public Segment(Vector3 pos)
+    {
+        prevPos = pos;
+        this.pos = pos;
+    }
 
-  public Segment(Segment segment)
-  {
-      pos = segment.pos;
-      prevPos = segment.prevPos;
-      velocity = segment.velocity;
-  }
-  public static Segment  SetPreviousPosition(Segment segment)
-  {
-      var temp = segment;
-      temp.prevPos = segment.pos;
-      segment = temp;
-      return segment;
-  }
- //  public Segment  SetPreviousPosition()
- //  {
- //      return new Segment(this.pos);
- //  }
- //  public static Segment SetPosition(Vector3 pos,Vector3 prevPos)
- // {
- //     var tmp = new Segment(pos, prevPos);
- //     return tmp;
- // }
- public  Segment SetPosition()
- {
-     var tmp = new Segment(this.pos, this.prevPos,this.velocity);
-     return tmp;
- }
-
- public Segment UpdatePosition(Vector3 velocity)
- {
-     var temp = new Segment(pos, prevPos,velocity);
-     temp.pos += velocity;
-     return temp;
- }
- public static Segment UpdatePosition(Segment segment,Vector3 velocity)
- {
-     var temp = segment;
-     temp.pos = segment.pos + velocity;
-     segment = temp;
-     return segment;
- }
-
- 
+    public Segment(Segment segment)
+    {
+        pos = segment.pos;
+        prevPos = segment.prevPos;
+    }
 }
